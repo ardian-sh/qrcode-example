b26d7cd48fc98fd21318eadcc39302353db325c9
ardiansah <ardiansah.info@gmail.com>
2022-04-19 16:26:04 +0700

    back commit

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,7 @@ namespace QrCodeGenerator
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsProduction())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
